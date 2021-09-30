fca4ea7e5c00a6c805cc49c7c40ab0f532b41732
kelvynking <kelvyn.m.king@gmail.com>
2021-09-30 16:13:04 +1000

    Added parameter 'city' and relevant ternary operator in the function ternaryCheckCity().

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function scuberGreetingForFeet(feet){
   }
 }
 
-function ternaryCheckCity(){
-  
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
