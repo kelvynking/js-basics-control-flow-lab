9211c423075338c9d7e989b29c72c2f888e1a7e4
kelvynking <kelvyn.m.king@gmail.com>
2021-09-30 16:07:12 +1000

    Added parameter 'feet' and relevant 'if..else if..else' statement in the function scuberGreetingForFeet.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }else if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
